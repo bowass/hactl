62924b47e711f4ac0dfadeeda3306b85f1538723
TomerGan <tomergan1@gmail.com>
2022-04-19 22:49:24 +0300

    Added very useful comments.

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -4,9 +4,9 @@
  * License: CC0
  * Source: me
  * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
- * Can be changed to other things.
+ * Can be changed to other things e.g. min, max, sum, xor, gcd, lcm (by replacing "max" function, replacing "val" neutral value, and sometimes other things specified).
  * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
- * Time: O(\log N).
+ * Time: O(\log N) query, O(N) non-sparse build
  * Usage: Node* tr = new Node(v, 0, sz(v));
  * Status: stress-tested a bit
  */
@@ -17,9 +17,11 @@
 const int inf = 1e9;
 struct Node {
 	Node *l = 0, *r = 0;
-	int lo, hi, mset = inf, madd = 0, val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
+	int lo, hi, mset = inf, madd = 0;
+	int val = -inf; // min: inf, max: -inf, sum: 0 or [init_value * (hi - lo) -> in constructor], gcd: 1 (init_value), lcm: 1 (init_value)
+	Node(int lo,int hi):lo(lo),hi(hi){} // sparse segment tree (use for large intervals of default value "val")
 	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
+		// build segment tree from v - [lo, hi)
 		if (lo + 1 < hi) {
 			int mid = lo + (hi - lo)/2;
 			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
@@ -27,26 +29,32 @@ struct Node {
 		}
 		else val = v[lo];
 	}
-	int query(int L, int R) {
-		if (R <= lo || hi <= L) return -inf;
+	int query(int L, int R) { // [L, R)
+		if (R <= lo || hi <= L) return -inf; // CHANGE THIS (out of range)
 		if (L <= lo && hi <= R) return val;
 		push();
 		return max(l->query(L, R), r->query(L, R));
 	}
-	void set(int L, int R, int x) {
+	void set(int L, int R, int x) { // [L, R)
 		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) mset = val = x, madd = 0;
+		if (L <= lo && hi <= R) {
+			// CHANGE THIS (update covers node)
+			mset = x;
+			val = x; // e.g. for sum: x * (hi - lo), for xor: x * ((hi - lo) & 1)
+			madd = 0;
+		}
 		else {
 			push(), l->set(L, R, x), r->set(L, R, x);
 			val = max(l->val, r->val);
 		}
 	}
-	void add(int L, int R, int x) {
+	void add(int L, int R, int x) { // [L, R)
 		if (R <= lo || hi <= L) return;
 		if (L <= lo && hi <= R) {
+			// CHANGE THIS (update covers node)
 			if (mset != inf) mset += x;
 			else madd += x;
-			val += x;
+			val += x; // e.g. for sum: x * (hi - lo), xor/gcd/lcm too hard
 		}
 		else {
 			push(), l->add(L, R, x), r->add(L, R, x);
